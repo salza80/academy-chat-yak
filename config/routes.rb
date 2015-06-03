41c593609f8b7d57bc487f673621155b533f2b2d
zkusznir <zkusznir@gmail.com>
2015-06-03 13:14:37 +0200

    Old login routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources  :messages, only: [:index, :new, :create]
   root 'messages#index'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: 'sessions#failure'
   delete 'logout', to: 'sessions#destroy'
