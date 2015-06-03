a35e9b39a907df63d92abe475190973fdda5bdbe
zkusznir <zkusznir@gmail.com>
2015-06-03 13:13:01 +0200

    Message list of currently logged in user displayed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :logged_in
 
   def index
-    @messages = Message.all
+    @messages = Message.all.where(user_id: session[:id])
     @message = Message.new
   end
 
