0fd6b2e9dbedd020825594e02340c872401d7517
Sally Mclean <smclean17@gmail.com>
2015-06-10 10:39:29 +0200

    Fix Indentation #18272827f58d3677469a939e14842c988fc111f9

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -22,35 +22,30 @@ Yak.Backend.prototype.error = function(response) {
 
 Yak.Backend.prototype.fetch = function(url) {
   return fetch(url, {credentials: 'include' })  
-          .then(this.status)  
-          .then(this.json)
-          .then(function(data) {
-            console.log('Get request succeeded with JSON response', data);  
-            return Promise.resolve(data);   
-          })
-          .catch(this.error);
+  .then(this.status)  
+  .then(this.json)
+  .then(function(data) {
+    console.log('Get request succeeded with JSON response', data);  
+    return Promise.resolve(data);   
+  })
+  .catch(this.error);
 };
 
 Yak.Backend.prototype.postJSON = function(url, jsonData) {
   return fetch(url, {  
-                      credentials: 'include',
-                      method: 'post',  
-                      headers: {  
-                                  'Accept': 'application/json',
-                                  'Content-Type': 'application/json',
-                                },  
-                      body: JSON.stringify(jsonData)
-                    }
-          )
-          .then(this.json)  
-          .then(function (data) {  
-            console.log('Post request succeeded with JSON response', data); 
-            return Promise.resolve(data) 
-          })  
-          .catch(this.error);
+    credentials: 'include',
+    method: 'post',  
+    headers: {  
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },  
+    body: JSON.stringify(jsonData)
+  })
+  .then(this.json)  
+  .then(function (data) {  
+    return Promise.resolve(data) 
+  })  
+  .catch(this.error);
 };
-
-
-
 
  
