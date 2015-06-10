1c5040ed162d5f31fba5f6a25ccca1fb170e7a5c
Sally Mclean <smclean17@gmail.com>
2015-06-10 10:27:14 +0200

    rename init-pusher.js init_pusher.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( init_rollbar.js pusher.min.js fetch.js es6-promise.min.js init-pusher.js )
+Rails.application.config.assets.precompile += %w( init_rollbar.js pusher.min.js fetch.js es6-promise.min.js)
