5bfd5aee3e6bebda6736036d7560942a777a890f
zkusznir <zkusznir@gmail.com>
2015-06-22 09:31:46 +0200

    Messages controller refactor

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,10 +2,8 @@ class Api::MessagesController < ApplicationController
   before_action :logged_in, :set_chat_room
 
   def index
-    messages = params[:last_id].nil? ? @chat_room.messages : find_older
-    @messages = messages.last(5)
-    first_message = @messages.first.id == @chat_room.messages.first.id
-    @all_messages = @messages.empty? || first_message ? true : false
+    @messages = before(params[:last_id]).last(20)
+    all_messages?
   end
 
   def create
@@ -23,8 +21,13 @@ class Api::MessagesController < ApplicationController
 
   private
 
-  def find_older
-    @chat_room.messages.select { |message| message.id < params[:last_id].to_i }
+  def before(last_id)
+    last_id ? @chat_room.messages.where('id < ?', last_id) : @chat_room.messages
+  end
+
+  def all_messages?
+    first_message = @messages.first.id == @chat_room.messages.first.id
+    @all_messages = @messages.empty? || first_message ? true : false
   end
 
   def message_params
