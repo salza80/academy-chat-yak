27053edb1dd4e02531df2b1b7dba93432e8f5efb
Sally Mclean <smclean17@gmail.com>
2015-07-03 11:07:34 +0200

    fix room is removed test

--- spec/features/chat_rooms_multi_session_spec.rb
+++ spec/features/chat_rooms_multi_session_spec.rb
@@ -20,8 +20,9 @@ feature 'pusher_actions' do
 
   scenario 'Room is removed from all sessions and active users in the room exit' do
     @session3 =  new_session_login 'user', '3'
+    @session1.find('.room-list-item', text: 'Empty Room').click
     @session2.find('.room-list-item', text: 'Empty Room').click
-    @session2.find('.room-list-item', text: 'Berlin').click
+    @session3.find('.room-list-item', text: 'Berlin').click
     @session1.find('.room-list-item', text: 'Empty Room').find('.glyphicon').click
     @session1.click_button('Yes')
     expect(@session1).not_to have_text('Are you sure?')
